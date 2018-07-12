4465f9b9e39a71e9e14c7c85d427b0c2cc8b1602
igorfromtomsk <igorfromtomsk@gmail.com>
2018-07-12 12:11:40 +0700

    + some pokemons + auth layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
